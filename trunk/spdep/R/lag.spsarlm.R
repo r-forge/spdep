d307fccd9764a91836806736bd57120482fa787d
rsb <unknown@noreply.com>
2002-03-19 21:28:16 +0000

    release
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spdep@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/spdep/R/lag.spsarlm.R
+++ trunk/spdep/R/lag.spsarlm.R
@@ -1,15 +1,5 @@
 # Copyright 1998-2002 by Roger Bivand and Andrew Bernat
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
 
 lagsarlm <- function(formula, data = list(), listw, type="lag",
 	method="eigen", quiet=TRUE, zero.policy=FALSE, tol.solve=1.0e-7, 
--- trunk/spdep/R/listw2sn.R
+++ trunk/spdep/R/listw2sn.R
@@ -1,15 +1,5 @@
 # Copyright 2001 by Roger Bivand
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
 
 listw2sn <- function(listw) {
 	if(class(listw) != "listw") stop("not a listw object")
