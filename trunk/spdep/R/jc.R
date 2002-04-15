c9c3f17deb7314e3b22de40ff7276c1501377932
rsb <unknown@noreply.com>
2002-04-15 16:35:34 +0000

    added sids
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spdep@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/spdep/R/jc.R
+++ trunk/spdep/R/jc.R
@@ -19,7 +19,7 @@ joincount <- function(dums, listw) {
 }
 
 joincount.test <- function(fx, listw,
-	alternative="greater") {
+	alternative="greater", zero.policy=FALSE) {
 	if (class(listw) != "listw") stop(paste(deparse(substitute(listw)),
 		"is not a listw object"))
 	if (!is.factor(fx)) stop(paste(deparse(substitute(x)),
@@ -27,7 +27,7 @@ joincount.test <- function(fx, listw,
 	if (any(is.na(fx))) stop("NA in factor")
 	n <- length(listw$neighbours)
 	if (n != length(fx)) stop("objects of different length")
-	wc <- spweights.constants(listw)
+	wc <- spweights.constants(listw, zero.policy)
 	S02 <- wc$S0*wc$S0
 
 	dums <- lm(codes(fx) ~ fx - 1, x=TRUE)$x
