f5b65e9502fed19648cba28cf61e874f924fc3d6
rsb <unknown@noreply.com>
2003-04-26 14:19:39 +0000

    EB and inherits
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spdep@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/spdep/R/nb2listw.R
+++ trunk/spdep/R/nb2listw.R
@@ -20,7 +20,7 @@ nb2listw <- function(neighbours, glist=NULL, style="W", zero.policy=FALSE)
 		attr(vlist, as.character(source)) <- TRUE
 	}
 	if (length(glist) != n) stop("glist wrong length")
-	if (cardnb != unlist(lapply(glist, length)))
+	if (any(cardnb != unlist(lapply(glist, length))))
 		stop("neighbours and glist do not conform")
 	if (any(is.na(unlist(glist))))
 		stop ("NAs in general weights list")
